bc1707a5dbc393a9582b87b05e4fe5b98aea973e
Allen Chen <allen.chen@autodesk.com>
2015-07-22 18:03:41 +0800

    -Add Round function to round the Utilization(percent) to int

---
+++ round.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"math"
+)
+
+func Round(f float64) float64 {
+	return math.Floor(f + .5)
+}
+
+func RoundPlus(f float64, places int) float64 {
+	shift := math.Pow(10, float64(places))
+	return Round(f*shift) / shift
+}
